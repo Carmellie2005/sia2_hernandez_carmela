f87a9dd7ab591a368b99bc9dc75313bc0c251d74
Carmela Hernandez <hernandezcarmelabihay@gmail.com>
2026-03-24 11:54:53 +0800

    feat: implement product MVC structure

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +29,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/products', [ProductController::class, 'index']);
